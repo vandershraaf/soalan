bb77ba9de2799e5b15ca2681d8c4843672457895
Muhammad Ashraf Ishak <m.ashraf.ishak@gmail.com>
2020-10-28 20:37:00 +0800

    Added meta tags

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,8 +4,15 @@
     <title>Quizzz</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Quiz made easier">
+    <meta name="author" content="Muhammad Ashraf Ishak">
+
+    <meta property="og:url"                content="https://quizzz.app" />
+    <meta property="og:type"               content="website" />
+    <meta property="og:title"              content="Quizzz" />
+    <meta property="og:description"        content="Quiz made easier" />
+    <meta property="og:image"              content="{{ asset('/favicon.ico') }}" />
+
     <link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/all.css') }}">
     <link rel="icon" href="{{ asset('/favicon.ico') }}">
