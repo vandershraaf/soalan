d21caa8eeb60fd11a68cba893a304d5e3c83d3b7
Muhammad Ashraf Ishak <m.ashraf.ishak@gmail.com>
2020-10-29 11:34:44 +0800

    Favicon changes

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -18,10 +18,14 @@
     <meta property="twitter:image"         content="{{ asset('images/android-chrome-192x192.png') }}" />
 
 
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("apple-touch-icon.png") }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("favicon-32x32.png") }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("favicon-16x16.png") }}">
+    <link rel="manifest" href="{{ asset("site.webmanifest") }}">
 
     <link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/all.css') }}">
-    <link rel="icon" href="{{ asset('/favicon.ico') }}">
+
 
     @livewireStyles
 
